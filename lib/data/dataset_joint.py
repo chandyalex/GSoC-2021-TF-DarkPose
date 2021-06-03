6dc5d38f5b3c17cdae936f743385ffa91294e786
chandy <chandyalex92@gmail.com>
2021-06-03 23:44:24 +0200

    more resrtucturing

--- lib/data/dataset_joint.py
+++ lib/data/dataset_joint.py
@@ -3,6 +3,7 @@
 # Licensed under the MIT License.
 # Written by Bin Xiao (Bin.Xiao@microsoft.com)
 # Modified by Hanbin Dai (daihanbin.ac@gmail.com)
+# Modified by Chandykunju Alex (chandyalex92@gmail.com)
 # ------------------------------------------------------------------------------
 
 from __future__ import absolute_import
@@ -12,13 +13,16 @@ from __future__ import print_function
 import copy
 import logging
 import random
+import sys
 
 import cv2
 import numpy as np
 import tensorflow as tf
 # from tf.data import Dataset
 #
-
+import sys
+sys.path.append("/home/chandy/gsoc/Darkpose_Tensorflow/lib")
+#### to be removed later
 from utils.transforms import get_affine_transform
 from utils.transforms import affine_transform
 from utils.transforms import fliplr_joints
